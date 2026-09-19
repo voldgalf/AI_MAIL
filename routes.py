e6c51cf186d1fbb5b9926d21bcd7c9edc7f76a3f
Voldgalf <50calibursword@gmail.com>
2026-09-19 09:31:36 -0400

    feat: added create-mailbox route

--- routes.py
+++ routes.py
@@ -19,3 +19,21 @@ def authenticate(database: database_dependency, request: RequestAuthenticate):
         existing_mailbox.id, existing_mailbox.address)
 
     return ResponseAuthenticate(address=existing_mailbox.address, jwt=session_token)
+
+
+@router.get("/create-mailbox", response_model=ResponseCreateMailbox)
+def create_mailbox(database: database_dependency, request: RequestCreateMailbox):
+    if (existing_mailbox := database.exec(select(Mailbox).where(Mailbox.address == request.address)).first()):
+        raise MailException("Mailbox already exists")
+    
+    hashed_password: bytes = bcrypt.hashpw(request.password.encode('utf-8'), bcrypt.gensalt())
+    
+    new_mailbox = Mailbox(address=request.address, password_hash=hashed_password)
+        
+    database.add(new_mailbox)
+    database.commit()
+    database.refresh(new_mailbox)
+    
+    response = ResponseCreateMailbox(mailbox=new_mailbox)
+    
+    return response
