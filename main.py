7c93ac06067dcbb4c0e0aabd480089e29bee9024
Voldgalf <50calibursword@gmail.com>
2026-09-18 22:46:06 -0400

    fix: removed git error

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from database import Mailbox, Message, database_dependency
 from classes import RequestAuthenticate, ResponseAuthenticate, ResponseBase
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
-
 from exception import MailException
-<<<<<<< HEAD
 
 app = FastAPI()
 
@@ -13,14 +11,3 @@ async def mail_exception_handler(request: Request, err: MailException):
     error_response = ResponseBase(
         success=False, message=err.message, data=None)
     return JSONResponse(status_code=200, content=error_response.model_dump())
-
-=======
-
-app = FastAPI()
-
-@app.exception_handler(MailException)
-async def mail_exception_handler(request: Request, err: MailException):
-    error_response = ResponseBase(
-        success=False, message=err.message, data=None)
-    return JSONResponse(status_code=200, content=error_response.model_dump())
->>>>>>> b3e04979dbe5a0cf43586d476e96e495fc88687c
