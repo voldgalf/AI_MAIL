bf49d467f6bf121f5d1186dfdd05bdc9ee510e47
Voldgalf <50calibursword@gmail.com>
2026-09-19 13:27:40 -0400

    feat: updated authenticate_dependency validate_mailbox logic to use engine_manager

--- depends.py
+++ depends.py
@@ -1,4 +1,4 @@
-from sqlmodel import Session, select
+from sqlmodel import Session
 from classes import RequestCredientals
 from fastapi import Depends
 from exception import MailException
@@ -7,20 +7,21 @@ from sessions import session_manager
 
 from database import engine_manager, Mailbox
 
+
 def create_database_session():
     with Session(engine_manager.sql_engine) as session:
         yield session
 
 
-database_dependency = Annotated[Session, Depends(create_database_session)]
+def validate_mailbox(request: RequestCredientals):
 
-def validate_mailbox(request: RequestCredientals, database: database_dependency):
-    if not (existing_mailbox := database.exec(select(Mailbox).where(Mailbox.address == request.address)).first()):
+    if not (existing_mailbox := engine_manager.get_mailbox_by_property("address", request.address)):
         raise MailException("Mailbox does not exist")
 
     if not (session_manager.validate_token(existing_mailbox.id, jwt=request.jwt)):
         raise MailException("Invalid session token")
-    
+
     return existing_mailbox
+
 
 authenticate_dependency = Annotated[Mailbox, Depends(validate_mailbox)]
