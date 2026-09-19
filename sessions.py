9463a9204a83fdd6e0a274ebf83b35bdaea2b855
Voldgalf <50calibursword@gmail.com>
2026-09-18 22:55:26 -0400

    feat: created SessionManager
    Manages session tokens using Redis

---
+++ sessions.py
@@ -0,0 +1,26 @@
+import redis, uuid, jwt, time
+
+from exception import MailException
+
+class SessionManager():
+    def __init__(self):
+        self.r = redis.Redis()
+        
+    def check_token(self, uuid: uuid.UUID) -> bool:
+        found_token = self.r.get(str(uuid))
+        
+        return (found_token != None)
+    
+    def create_token(self, uuid: uuid.UUID, address: str) -> str:
+        
+        if(self.check_token(uuid)):
+            raise MailException("Already existing session token!")
+            
+        new_token = jwt.encode({"address": address, "uuid": str(uuid), "created_at":time.time()}, "secret", algorithm="HS256")
+        
+        self.r.set(str(uuid), new_token)
+        
+        return new_token
+    
+    
+session_manager = SessionManager()
